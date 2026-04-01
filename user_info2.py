8bc07aa164906825a3caaec5b67f745e81b47f52
Bogdan Kogut <work.bohdan.kohut@gmail.com>
2026-04-01 16:19:12 +0300

    day 4 dict and user manager

---
+++ user_info2.py
@@ -0,0 +1,11 @@
+user = {
+    "name": "Oleg Gazon Volodymyr Mykolaiovich",
+    "age": 31,
+    "city": "Golopyzdynzsk",
+    "companion": "Dasha"
+}
+
+print(user["name"])
+print(user["age"])
+print(user["city"])
+print(user["companion"])
---
+++ users_list.py
@@ -0,0 +1,8 @@
+users = [
+    {"name": "Bohdan", "age": 31, "city": "Kyiv"}
+    {"name": "Ivan", "age": 21, "city": "Lviv"}
+    {"name": "Olena", "age": 23, "city": "Odessa"}
+]
+
+for user in users:
+    print(user["name"], user["age"])
